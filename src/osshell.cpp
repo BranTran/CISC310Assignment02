5a1e5ec222143214384fda965bdef12e2e37f5a4
BranTran <tran2833@stthomas.edu>
2020-02-14 09:20:33 -0600

    Read through source with notes

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -14,14 +14,32 @@ int main (int argc, char **argv)
     std::vector<std::string> os_path_list = splitString(os_path, ':');
 
     std::cout << "Welcome to OSShell! Please enter your commands ('exit' to quit)." << std::endl;
+//Load in history
+
+
 
     // Repeat:
     //  Print prompt for user input: "osshell> " (no newline)
+y
+
     //  Get user input for next command
+
+
     //  If command is `exit` exit loop / quit program
+
+
     //  If command is `history` print previous N commands
+
+//Additional options
+
     //  For all other commands, check if an executable by that name is in one of the PATH directories
+
+//getFullPath();
+//fileExists();
+
     //   If yes, execute it
+
+
     //   If no, print error statement: "<command_name>: Error running command" (do include newline)
 
     return 0;
@@ -30,6 +48,7 @@ int main (int argc, char **argv)
 // Returns vector of strings created by splitting `text` on every occurance of `d`
 std::vector<std::string> splitString(std::string text, char d)
 {
+//NEED TO CODE
     std::vector<std::string> result;
 
     return result;
@@ -38,6 +57,7 @@ std::vector<std::string> splitString(std::string text, char d)
 // Returns a string for the full path of a command if it is found in PATH, otherwise simply return ""
 std::string getFullPath(std::string cmd, const std::vector<std::string>& os_path_list)
 {
+//THING TO DO
     return "";
 }
 
@@ -45,6 +65,7 @@ std::string getFullPath(std::string cmd, const std::vector<std::string>& os_path
 // depending on if the user has permission to execute the file
 bool fileExists(std::string full_path, bool *executable)
 {
+//THING TO DO
     *executable = false;
     return false;
 }
